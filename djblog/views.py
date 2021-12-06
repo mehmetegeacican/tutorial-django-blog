3c35ff86b6f40f2ae186e87a13d2365a48f6e56e
mehmet_ege_acican <ege.acican@onbiron.com.tr>
2021-12-06 14:54:42 +0300

    Django tutorial project initialized

---
+++ djblog/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from .models import Post
+
+
+#Static Data
+posts = [
+    {
+        "author": "Corey Schafer",
+        "title": "Blog Post #1",
+        "content": "The first post content",
+        "date": "December 3, 2021"
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Blog Post #2",
+        "content": "The second post content",
+        "date": "December 3, 2021"
+    },
+    
+]
+# Create your views here.
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html',context)
+
+def about(request):
+    return render(request,"./blog/about.html",{'title': "About"})
