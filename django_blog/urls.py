2a9e99ff7942e5411069909856e7c110a079d58f
mehmet_ege_acican <ege.acican@onbiron.com.tr>
2021-12-09 15:11:58 +0300

    User Profile section has been added

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.conf import Settings, settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("djblog.urls")),
@@ -24,5 +27,7 @@ urlpatterns = [
     path('sign_in/',auth_views.LoginView.as_view(template_name = "users/sign_in.html"), name="signin"),
     path('profile/',user_views.profile, name="profile"),
     path('sign_out/',auth_views.LogoutView.as_view(template_name = "users/sign_out.html"), name="signout"),
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
